a59d50f11e12e54fff17dae74a951ac157d6c5ba
Alec Lownes <alec.lownes@gmail.com>
2017-01-26 10:39:28 -0500

    Add category to models

--- models.js
+++ models.js
@@ -18,6 +18,11 @@ module.exports = function(sequelize, DataTypes) {
 			type: DataTypes.STRING,
 			allowNull: false,
 			unique: true
+		},
+		category: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			defaultValue: 'Other'
 		}
 	});
 
